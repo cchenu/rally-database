53583165fa4996417471d71f4386df3d214b1f7c
cchenu <clem.chenu@laposte.net>
2025-12-10 14:06:43 +0100

    Add session state

--- app/app.py
+++ app/app.py
@@ -11,28 +11,43 @@ ROOT = Path(__file__).resolve().parents[1]
 sys.path.append(str(ROOT))
 # -------------
 
-home_page = st.Page(
-    "home.py", title="Rally application", icon=":material/home:"
-)
-exercise_page = st.Page(
-    "exercise.py", title="Exercise", icon=":material/assignment:"
-)
-rally_page = st.Page(
-    "rally.py", title="Rally", icon=":material/directions_car:"
-)
-stage_page = st.Page("stage.py", title="Stage", icon=":material/map:")
-contestant_page = st.Page(
-    "contestant.py", title="Contestant", icon=":material/person:"
-)
 
-st.set_page_config(layout="wide")
+def create_app() -> None:
+    home_page = st.Page(
+        "home.py", title="Rally application", icon=":material/home:"
+    )
+    exercise_page = st.Page(
+        "exercise.py", title="Exercise", icon=":material/assignment:"
+    )
+    rally_page = st.Page(
+        "rally.py", title="Rally", icon=":material/directions_car:"
+    )
+    stage_page = st.Page("stage.py", title="Stage", icon=":material/map:")
+    contestant_page = st.Page(
+        "contestant.py", title="Contestant", icon=":material/person:"
+    )
 
-pg = st.navigation(
-    {
-        "Hub": [home_page],
-        "Assignment": [exercise_page],
-        "Reports": [rally_page, stage_page, contestant_page],
-    }
-)
+    st.set_page_config(layout="wide")
 
-pg.run()
+    pg = st.navigation(
+        {
+            "Hub": [home_page],
+            "Assignment": [exercise_page],
+            "Reports": [rally_page, stage_page, contestant_page],
+        }
+    )
+
+    if "id_rally" not in st.session_state:
+        st.session_state["id_rally"] = 576
+
+    if "id_contestant" not in st.session_state:
+        st.session_state["id_contestant"] = 419
+
+    if "id_stage" not in st.session_state:
+        st.session_state["id_stage"] = 326
+
+    pg.run()
+
+
+if __name__ == "__main__":
+    create_app()
